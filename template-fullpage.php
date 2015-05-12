751d50baefda769ff73232f024a12289fbb6da92
Francois Faubert <francois.faubert@iprospect.com>
2015-05-12 13:25:21 -0400

    moved all the files

--- template-fullpage.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Template Name: Full Page
-*/
-?>
-
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
--- templates/searchform.php
+++
@@ -1,9 +0,0 @@
-<form role="search" method="get" class="search-form form-inline" action="<?php echo home_url('/'); ?>">
-  <div class="input-group">
-    <input type="search" name="s" id="s" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" x-webkit-speech tabindex="1">
-    <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
-    <span class="input-group-btn">
-      <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'roots'); ?></button>
-    </span>
-  </div>
-</form>
