51e806cd7595f9bff64b77126e482296f0be9f9c
Francois Faubert <francois.faubert@iprospect.com>
2015-07-21 13:07:36 -0400

    autoload acf configs

--- web/app/mu-plugins/ip-bootstraper.php
+++ web/app/mu-plugins/ip-bootstraper.php
@@ -35,3 +35,24 @@ function auto_initialize_ip_modules()
 }
 
 add_action("plugins_loaded", "auto_initialize_ip_modules");
+
+
+
+add_filter('acf/settings/save_json', 'ip_auto_save_acf');
+function ip_auto_save_acf($path)
+{
+    $path = \Strata\Strata::getConfigurationPath() . 'acf';
+    if (!is_dir($path)) {
+        mkdir($path);
+    }
+
+    return $path;
+}
+
+add_filter('acf/settings/load_json', 'ip_auto_load_acf');
+function ip_auto_load_acf($paths)
+{
+    unset($paths[0]);
+    $paths[] = \Strata\Strata::getConfigurationPath() . 'acf';
+    return $paths;
+}
