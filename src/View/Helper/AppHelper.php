814a381d5ff7bf87d7375e0ba5ea9a543c8cf110
Francois Faubert <frank@francoisfaubert.com>
2015-07-26 19:35:41 -0400

    Create AppHelper.php

---
+++ src/View/Helper/AppHelper.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\View\Helper;
+
+/** This is the base view helper class for your application.
+ *  All your custom helper classes should extend this class.
+ *
+ */
+class AppHelper extends \Strata\View\Helper\Helper {
+
+
+}
