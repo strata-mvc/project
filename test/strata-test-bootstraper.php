c4e460584dc547f503f36b005fc6d07cf57f831f
Francois Faubert <frank@francoisfaubert.com>
2015-07-26 19:48:20 -0400

    Create strata-test-bootstraper.php

---
+++ test/strata-test-bootstraper.php
@@ -0,0 +1,13 @@
+<?php
+    $loader = include("vendor/autoload.php");
+    use \Strata\Strata;
+
+    Strata::includeWordpressFixture();
+
+    $app = Strata::bootstrapTest($loader);
+    $app->run();
+
+    // Customize the bootstrap file to suite your needs.
+    // In theory, you should not need to edit the lines above
+    // and would only need to include other bootstrap files
+    // under this paragraph.
