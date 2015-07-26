f6b88abd76527055fc460af4ae91b4253a2befcf
Francois Faubert <frank@francoisfaubert.com>
2015-07-26 19:34:13 -0400

    Create AppModel.php

---
+++ src/Model/AppModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Model;
+
+/** This is the base model class for your application.
+ *  All of your custom models should extend this class.
+ *
+ *  Model documentation can be found here :
+ *  http://strata.francoisfaubert.com/docs/models/
+ */
+class AppModel extends \Strata\Model\Model {
+
+
+}
