40e1ab04f8018af52efabddf470a580648666ffb
Francois Faubert <francois.faubert@iprospect.com>
2016-05-27 11:03:50 -0400

    bumped wordpress, fixed tests and simplified the strata config file

--- config/strata.php
+++ config/strata.php
@@ -2,22 +2,15 @@
 $strata = array(
 
     "routes" => array(
-        array('GET|POST',   '/[.*]',       'AppController#index'),
-        // Here are example values:
-        // array('GET|POST',   '/participate/volunteer/',     'VolunteersController#create'),
-        // array('POST',       '/wp-admin/admin-ajax.php',    'AjaxController#index'),
-        // array('GET',        '/schools/[*:slug]/',  'SchoolsController#view'),
+        array('POST', '/wp/wp-admin/admin-ajax.php', 'AjaxController'),
+        array('GET|POST', '/', 'AppController#home'),
+        // array('GET|POST',   '/[*:controller]/[*:action]'),
+        // array('GET|POST',   '/[*:controller]/'),
+        // array('GET|POST',   '/[.*]',       'AppController#noActionMatch'),
     ),
 
-    // Automate the creation of backend based post types.
-    // Added models your app will use that are based on the MVC CustomPostType\Entity class.
     "custom-post-types" => array(
-      // Exemples:
-      //  "School",
-      //  "Volunteer",
     )
 );
-
-// Additionally, you can manipulate the array further before returning it.
 
 return $strata;
--- test/strata-test-bootstraper.php
+++ test/strata-test-bootstraper.php
@@ -1,10 +1,4 @@
 <?php
-    $app = \Strata\Strata::bootstrap(include("vendor/autoload.php"));
-
-    $app->init();
-    $app->includeWordpressFixture();
-    $app->includeGettextFixture();
-    $app->run();
 
     // Customize the bootstrap file to suite your needs.
     // In theory, you should not need to edit the lines above
